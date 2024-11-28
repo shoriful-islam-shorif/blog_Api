7eb9b6f75cb39f28c1eacc45e11a0180b49e1751
shoriful-islam-shorif <shoriful.islam5435@gmail.com>
2024-11-28 17:18:21 +0600

    update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -16,12 +16,13 @@ use Intervention\Image\Facades\Image;
 
 class BlogController extends Controller
 {
-    public function index()
+    public function index($category_id)
 {
    
     $blogs = Blog::join('categories', 'categories.id', '=', 'blogs.category_id')
         ->select('blogs.*', 'categories.name as category_name')
         ->where('blogs.status', 1)
+        ->where('blogs.category_id',$category_id )
         ->orderby('blogs.created_at', 'desc')
         ->paginate(8); // or you can use get() for all blogs in that category
 
